0d1988bf12131ba1614cadef7f36b87226299123
Quim Muntal <quimmuntal@gmail.com>
2018-12-02 19:05:24 +0100

    add compressionoption enum

--- part.go
+++ part.go
@@ -1,7 +1,27 @@
 package gopc
 
+// CompressionOption is an enumerable for the different compression options.
+type CompressionOption int
+
+const (
+	// CompressionNone disables the compression.
+	CompressionNone CompressionOption = iota - 1
+	// CompressionNormal is optimized for a reasonable compromise between size and performance.
+	CompressionNormal
+	// CompressionMaximum is optimized for size.
+	CompressionMaximum
+	// CompressionFast is optimized for performance.
+	CompressionFast
+	// CompressionSuperFast is optimized for super performance.
+	CompressionSuperFast
+)
+
 // Part defines an OPC Package Object.
 type Part struct {
 	uri           string
 	relationships []Relationship
+}
+
+func newPart(uri, contentType string, compressionOption CompressionOption) {
+
 }
