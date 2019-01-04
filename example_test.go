08c2a1b9b927bd929cc6a1f18cd0352962fe98ba
Quim Muntal <quimmuntal@gmail.com>
2019-01-04 17:17:04 +0100

    add writer example

---
+++ example_test.go
@@ -0,0 +1,34 @@
+package gopc_test
+
+import (
+	"bytes"
+	"log"
+
+	"github.com/qmuntal/gopc"
+)
+
+func ExampleWriter() {
+	// Create a buffer to write our archive to.
+	buf := new(bytes.Buffer)
+
+	// Create a new OPC archive.
+	w := gopc.NewWriter(buf)
+
+	// Create a new OPC part.
+	part, err := w.Create("/readme.txt", "text/plain", gopc.CompressionNormal)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Write content to the part.
+	_, err = part.Write([]byte("This archive contains some text files."))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Make sure to check the error on Close.
+	err = w.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
