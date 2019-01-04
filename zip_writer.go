d3dbb42a2f4f32e49fb00247e318bf0307af1c6e
Quim Muntal <quimmuntal@gmail.com>
2019-01-04 15:41:24 +0100

    add package writer

---
+++ zip_writer.go
@@ -0,0 +1,121 @@
+package gopc
+
+import (
+	"archive/zip"
+	"compress/flate"
+	"errors"
+	"io"
+	"time"
+)
+
+// Writer implements a OPC file writer.
+type Writer struct {
+	Package *Package
+	w       *zip.Writer
+	last    *Part
+}
+
+// NewWriter returns a new Writer writing an OPC file to w.
+func NewWriter(w io.Writer) *Writer {
+	return &Writer{Package: newPackage(), w: zip.NewWriter(w)}
+}
+
+// Flush flushes any buffered data to the underlying writer.
+// Part metadata, relationships, content types and other OPC related files won't be flushed.
+// Calling Flush is not normally necessary; calling Close is sufficient.
+// Useful to do simultaneos writing and reading.
+func (w *Writer) Flush() error {
+	return w.w.Flush()
+}
+
+// Close finishes writing the opc file.
+// It does not close the underlying writer.
+func (w *Writer) Close() error {
+	if w.last != nil {
+		w.last.writeRelationships(nil)
+		w.last = nil
+	}
+	return w.w.Close()
+}
+
+// Create adds a new Part to the Package. The part's contents must be written before the next call to Create, Copy or Close.
+// The part URI shall be a valid part name, one can use NormalizePartName before calling Create to normalize the URI as a part name.
+func (w *Writer) Create(uri, contentType string, compressionOption CompressionOption) (*Part, error) {
+	part, err := w.Package.create(uri, contentType, compressionOption)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = w.add(part); err != nil {
+		return nil, err
+	}
+	return part, nil
+}
+
+// Copy an existing readable Part to the Package.
+// The Part will be read until EOF and it won't be readable again.
+func (w *Writer) Copy(part *Part) error {
+	if part.r != nil {
+		return errors.New("OPC: part cannot be copied because it does not have read access")
+	}
+	if err := w.Package.add(part); err != nil {
+		return err
+	}
+	if err := w.add(part); err != nil {
+		return err
+	}
+	if _, err := io.Copy(part.w, part.r); err != nil {
+		return err
+	}
+	part.r = nil
+	return nil
+}
+
+func (w *Writer) add(part *Part) error {
+	if w.last != nil {
+		w.last.writeRelationships(nil)
+	}
+
+	fh := &zip.FileHeader{
+		Name:     part.uri[1:], // remove first "/"
+		Modified: time.Now(),
+	}
+	w.setCompressor(fh, part.compressionOption)
+	pw, err := w.w.CreateHeader(fh)
+	if err != nil {
+		w.Package.deletePart(part.uri)
+		return err
+	}
+	part.w = pw
+	w.last = part
+	return nil
+}
+
+func (w *Writer) setCompressor(fh *zip.FileHeader, compression CompressionOption) {
+	var comp int
+	switch compression {
+	case CompressionNormal:
+		comp = flate.DefaultCompression
+	case CompressionMaximum:
+		comp = flate.BestCompression
+		fh.Flags |= 0x2
+	case CompressionFast:
+		comp = flate.BestSpeed
+		fh.Flags |= 0x4
+	case CompressionSuperFast:
+		comp = flate.BestSpeed
+		fh.Flags |= 0x6
+	case CompressionNone:
+		fh.Method = zip.Store
+		return
+	}
+
+	fh.Method = zip.Deflate
+	w.w.RegisterCompressor(zip.Deflate, compressionFunc(comp))
+}
+
+func compressionFunc(comp int) func(out io.Writer) (io.WriteCloser, error) {
+	return func(out io.Writer) (io.WriteCloser, error) {
+		return flate.NewWriter(out, comp)
+	}
+}
---
+++ zip_writer_test.go
@@ -0,0 +1,109 @@
+package gopc
+
+import (
+	"archive/zip"
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+func TestNewWriter(t *testing.T) {
+	tests := []struct {
+		name  string
+		want  *Writer
+		wantW string
+	}{
+		{"base", &Writer{Package: newPackage(), w: zip.NewWriter(&bytes.Buffer{})}, ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := &bytes.Buffer{}
+			if got := NewWriter(w); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewWriter() = %v, want %v", got, tt.want)
+			}
+			if gotW := w.String(); gotW != tt.wantW {
+				t.Errorf("NewWriter() = %v, want %v", gotW, tt.wantW)
+			}
+		})
+	}
+}
+
+func TestWriter_Flush(t *testing.T) {
+	tests := []struct {
+		name    string
+		w       *Writer
+		wantErr bool
+	}{
+		{"base", NewWriter(&bytes.Buffer{}), false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.w.Flush(); (err != nil) != tt.wantErr {
+				t.Errorf("Writer.Flush() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestWriter_Close(t *testing.T) {
+	tests := []struct {
+		name    string
+		w       *Writer
+		wantErr bool
+	}{
+		{"base", NewWriter(&bytes.Buffer{}), false},
+		{"withLast", &Writer{Package: newPackage(), w: zip.NewWriter(&bytes.Buffer{}), last: &Part{relationer: relationer{}}}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.w.Close(); (err != nil) != tt.wantErr {
+				t.Errorf("Writer.Close() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestWriter_setCompressor(t *testing.T) {
+	type args struct {
+		fh          *zip.FileHeader
+		compression CompressionOption
+	}
+	tests := []struct {
+		name       string
+		w          *Writer
+		args       args
+		wantFlag   uint16
+		wantMethod uint16
+	}{
+		{"none", NewWriter(nil), args{&zip.FileHeader{}, CompressionNone}, 0x0, zip.Store},
+		{"normal", NewWriter(nil), args{&zip.FileHeader{}, CompressionNormal}, 0x0, zip.Deflate},
+		{"max", NewWriter(nil), args{&zip.FileHeader{}, CompressionMaximum}, 0x2, zip.Deflate},
+		{"fast", NewWriter(nil), args{&zip.FileHeader{}, CompressionFast}, 0x4, zip.Deflate},
+		{"sfast", NewWriter(nil), args{&zip.FileHeader{}, CompressionSuperFast}, 0x6, zip.Deflate},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.w.setCompressor(tt.args.fh, tt.args.compression)
+			if tt.args.fh.Method != tt.wantMethod {
+				t.Error("Writer.setCompressor() should have set the method flag the deflate")
+			}
+		})
+	}
+}
+
+func Test_compressionFunc(t *testing.T) {
+	type args struct {
+		comp int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"base", args{1}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			compressionFunc(tt.args.comp)(&bytes.Buffer{})
+		})
+	}
+}
