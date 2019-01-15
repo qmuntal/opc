cfc8e7729e487421fc73dc764e9e1b85e11f073e
Carlota Catot Bragós <carlota.catot@est.fib.upc.edu>
2019-01-15 12:21:01 +0100

    added some control error

--- reader.go
+++ reader.go
@@ -33,39 +33,47 @@ func (r *Reader) loadParts() {
 func (r *Reader) getContentType() error {
 	// Process descrived in ISO/IEC 29500-2 §10.1.2.4
 	files := r.r.File
+	var found bool
 	for i := 0; i < len(files); i++ {
-		if files[i].Name == "[Content_Types].xml" {
-			reader, err := files[i].Open()
-			if err != nil {
-				return err
-			}
-			ct, err := decodeContentTypes(reader)
-			if err != nil {
-				return err
-			}
+		if files[i].Name != "[Content_Types].xml" {
+			continue
+		}
 
-			match := false
+		found = true
+		reader, err := files[i].Open()
+		if err != nil {
+			return err
+		}
+		ct, err := decodeContentTypes(reader)
+		if err != nil {
+			return err
+		}
 
-			for _, c := range ct.Types {
-				if cOverride, ok := c.(overrideContentTypeXML); ok {
-					for j := 0; j < len(files); j++ {
-						if r.Parts[j].Name == cOverride.PartName {
-							r.Parts[j].ContentType = cOverride.ContentType
-							match = true
-						}
-					}
-				} else if cDefault, ok := c.(defaultContentTypeXML); ok && !match {
-					ext := cDefault.Extension
-					for j := 0; j < len(files); j++ {
-						if filepath.Ext(r.Parts[j].Name)[1:] == ext {
+		for _, c := range ct.Types {
+			if cDefault, ok := c.(defaultContentTypeXML); ok {
+				ext := cDefault.Extension
+				for j := 0; j < len(files); j++ {
+					if filepath.Ext(r.Parts[j].Name)[1:] == ext {
+						if r.Parts[j].ContentType == "" {
 							r.Parts[j].ContentType = cDefault.ContentType
+						} else {
+							return errors.New("OPC: there must be only one Default content type for each extension")
 						}
 					}
-				} else {
-					return errors.New("OPC: contant types has a element with an unexpected type")
 				}
+			} else if cOverride, ok := c.(overrideContentTypeXML); ok {
+				for j := 0; j < len(files); j++ {
+					if r.Parts[j].Name == cOverride.PartName {
+						r.Parts[j].ContentType = cOverride.ContentType
+					}
+				}
+			} else {
+				return errors.New("OPC: content types has a element with an unexpected type")
 			}
 		}
+	}
+	if !found {
+		return errors.New("OPC: the file content type must exist in the package")
 	}
 	return nil
 }
